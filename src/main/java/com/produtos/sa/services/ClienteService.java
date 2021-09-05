21bbbcc756d3aee7ab52dc469c9e374ef3bf71df
gtoledo-dev <glauco_toledo@yahoo.com.br>
2021-09-05 12:53:38 -0300

    Tratamento de excessão do método deletar

--- src/main/java/com/produtos/sa/services/ClienteService.java
+++ src/main/java/com/produtos/sa/services/ClienteService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.produtos.sa.entities.Cliente;
 import com.produtos.sa.repositories.ClienteRepository;
+import com.produtos.sa.services.exceptions.DatabaseException;
 import com.produtos.sa.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class ClienteService {
 	}
 	
 	public void deletar(Long id) {
-		clienteRepository.deleteById(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public Cliente atualizar(Long id, Cliente obj) {
